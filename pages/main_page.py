ebfe76247ac3b9f696eb9000c5ff952e3d2b0d76
unknown <mark.avdiev@gmail.com>
2021-11-23 15:32:35 +0600

    временно поставили заглушку

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,7 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
-from .locators import MainPageLocators
 from .login_page import LoginPage
 
 class MainPage(BasePage):
-    def go_to_login_page(self):
-        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
-        link.click()
-        # return LoginPage(browser=self.browser, url=self.browser.current_url)
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
