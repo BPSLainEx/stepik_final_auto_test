c18984c745d7a9cb400ec03c3ab3af8bd9e5ffb2
BPSLainEx <93445306+BPSLainEx@users.noreply.github.com>
2021-11-25 14:47:36 +0600

    все наши селекторы

--- pages/locators.py
+++ pages/locators.py
@@ -10,6 +10,15 @@ class ProductPageLocators(object):
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+class BasketPageLocators():
+    BASKET_PAGE = (By.CSS_SELECTOR, 'span.btn-group')
+    BASKET_CONTENT = (By.CSS_SELECTOR, 'div.basket-title hidden-xs')
+    BASKET_MESSAGE = (By.CSS_SELECTOR, 'div#content_inner p')
 class LoginPageLocators():
     login_form = (By.CSS_SELECTOR, '#login_form')
     register_form = (By.CSS_SELECTOR, '#register_form')
+    register_email = (By.NAME, 'registration-email')
+    register_password1 = (By.NAME, 'registration-password1')
+    register_password2 = (By.NAME, 'registration-password2')
+    register_button = (By.NAME, 'registration_submit')
