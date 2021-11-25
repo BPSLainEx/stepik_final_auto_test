d8d47650a7262f3feea898d07729275cfc3ea03c
BPSLainEx <93445306+BPSLainEx@users.noreply.github.com>
2021-11-25 14:50:03 +0600

    методы проверок и регистрации для страниц логина

--- pages/login_page.py
+++ pages/login_page.py
@@ -6,7 +6,10 @@ class LoginPage(BasePage):
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
-
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+        return LoginPage(browser=self.browser, url=self.browser.current_url)
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
         assert (self.url.find('login') != -1), "Login is url not found"
@@ -17,3 +20,12 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.register_form), "Login register is not found"
+    def register_new_user(self, email, password):
+        new_email = self.browser.find_element(*LoginPageLocators.register_email)
+        new_email.send_keys(email)
+        new_password1 = self.browser.find_element(*LoginPageLocators.register_password1)
+        new_password1.send_keys(password)
+        new_password2 = self.browser.find_element(*LoginPageLocators.register_password2)
+        new_password2.send_keys(password)
+        button = self.browser.find_element(*LoginPageLocators.register_button)
+        button.click()
