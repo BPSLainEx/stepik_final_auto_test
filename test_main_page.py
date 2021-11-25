0edc9d62d3a72177c240c2b65e996912024010f5
unknown <mark.avdiev@gmail.com>
2021-11-25 13:00:44 +0600

    тест для проверки корзины

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,12 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)
+from .pages.basket_page import BasketPage
+import pytest
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/'
+    page = BasketPage(browser, link)
     page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+    page.press_to_button_basket()
+    page.should_be_basket_content()
+    page.should_be_basket_message()
+
